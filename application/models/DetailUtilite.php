4349b4f342d8c7af274545d3dfbc274148288727
MichaelRandria <mrandrimizara@gmail.com>
2022-06-23 18:51:11 +0300

    Update DetailUtilite.php

--- application/models/DetailUtilite.php
+++ application/models/DetailUtilite.php
@@ -1,7 +1,7 @@
 <?php
     class DetailUtilite extends MY_Model{
         public function insert($id_publication,$id_utilite){
-            $sql="INSERT INTO Detail_utilite(id_detail_tag,id_tag,id_publication) VALUES (default,%s,%s)";
+            $sql="INSERT INTO Detail_utilite(id_detail_utilite,id_publication,id_utilite) VALUES (default,%s,%s)";
             $sql=sprintf($sql,$this->db->escape($id_publication),$this->db->escape($id_utilite));
             $this->db->query($sql);
         }
