cadbaeefd3328100d2fb7ba44002adae000cd8cf
MichaelRandria <mrandrimizara@gmail.com>
2022-06-24 18:42:34 +0300

    Update Photo.php

--- application/models/Photo.php
+++ application/models/Photo.php
@@ -1,8 +1,8 @@
 <?php
     class Client extends MY_Model{
-        public function insert($lien_photo, $id_publication){
-            $sql="INSERT INTO Photo(id_photo,lien_photo,id_publication) VALUES (default,%s,%s)";
-            $sql=sprintf($sql,$this->db->escape($lien_photo),$this->db->escape($id_publication));
+        public function insert($id_img,$lien_photo, $id_publication){
+            $sql="INSERT INTO Photo(id_photo,lien_photo,id_publication) VALUES (%s,%s,%s)";
+            $sql=sprintf($sql,$this->db->escape($id_img),$this->db->escape($lien_photo),$this->db->escape($id_publication));
             $this->db->query($sql);
         }
     }
