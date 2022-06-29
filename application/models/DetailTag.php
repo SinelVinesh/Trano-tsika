44e5fc1a0bf6332a25a471b771d8574d7f7c3945
Nancia <99491515+Nancia1586@users.noreply.github.com>
2022-06-29 10:12:39 +0300

    Update DetailTag.php

--- application/models/DetailTag.php
+++ application/models/DetailTag.php
@@ -5,5 +5,11 @@
             $sql=sprintf($sql,$this->db->escape($id_tag),$this->db->escape($id_publication));
             $this->db->query($sql);
         }
+
+        public function get_all(){
+            $sql = "SELECT * FROM v_detail_tags";
+            $query = execute_query($sql);
+            return $query->result_array();
+        }
     }
 ?>
