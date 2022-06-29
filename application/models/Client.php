c906b4708257f171d2c527c9c12b2a8296829ff1
Nancia <99491515+Nancia1586@users.noreply.github.com>
2022-06-29 10:12:34 +0300

    Update Client.php

--- application/models/Client.php
+++ application/models/Client.php
@@ -3,5 +3,11 @@
         public function exist($email, $password){
             $this->get_row("Client","Email='$email' and password=md5('$password')");
         }
+
+        public function insert($first_name,$last_name,$date_birth,$phone,$email,$password)
+        {
+            $sql = "insert into Client(first_name,last_name,date_birth,phone,email,password) values('".$first_name."','".$last_name."','".$date_birth."','".$phone."','".$email."',md5('".$password."'));";
+            $this->db->query($sql);
+        }
     }
 ?>
