6d1b0b8f17ae31e5bf7e90f2325e15f2cf45c7e4
MichaelRandria <mrandrimizara@gmail.com>
2022-07-07 22:12:01 +0300

    Ajout pub Done

--- application/models/DetailUtilite.php
+++ application/models/DetailUtilite.php
@@ -1,6 +1,6 @@
 <?php
     class DetailUtilite extends MY_Model{
-        public function insert($id_publication,$id_utilite){
+        public function insert($id_utilite,$id_publication){
             $sql="INSERT INTO Detail_utilite(id_detail_utilite,id_publication,id_utilite) VALUES (default,%s,%s)";
             $sql=sprintf($sql,$this->db->escape($id_publication),$this->db->escape($id_utilite));
             $this->db->query($sql);
--- application/models/Photo.php
+++ application/models/Photo.php
@@ -3,7 +3,6 @@
         public function insert($id_img,$lien_photo, $id_publication){
             $sql="INSERT INTO Photo(id_photo,lien_photo,id_publication) VALUES (%s,%s,%s)";
             $sql=sprintf($sql,$this->db->escape($id_img),$this->db->escape($lien_photo),$this->db->escape($id_publication));
-            echo $sql;
             $this->db->query($sql);
         }
     }
