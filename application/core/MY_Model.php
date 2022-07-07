8eb3182a0949d87a8f9cb034aae956d5d29960c4
ravaka34 <ravakanyaina34@gmail.com>
2022-07-07 15:59:39 +0300

    configuration

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -24,34 +24,6 @@
             $sql = "select * from $table_name where  $id_column=$id_value ";
             return $this->execute_query($sql)->row_array();
         }
-
-        // protected function insert($table_name, $datas, $column_names=null){
-        //     if($column_names != null && count($column_names) != count($datas)){
-        //         throw new Exception("Size of column names is different from the datas array");
-        //     }
-        //     $sql = "";
-        //     $value_question_marks = "";
-            
-        //     for ($i=0; $i < count($datas); $i++) { 
-        //         $value_question_marks += ",?";
-        //     }
-            
-        //     if($column_names == null){
-                
-        //         $sql = "insert into $table_name values (default $value_question_marks ) ";
-        //         $this->db->query($sql,$datas);
-        //     } else {
-        //         $name_question_marks = "";
-
-        //         for ($i=0; $i < count($column_names);$i++){
-        //             $name_question_marks += ",?";
-        //         }
-
-        //         $sql = "insert into $table_name values ()  "
-
-        //     }
-        // }
-
     }
 
 
