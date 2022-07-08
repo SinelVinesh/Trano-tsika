0123b01e6913201b14883e4adbd6c851f3e0f178
TsinjoAH <52tsinjo@gmail.com>
2022-07-08 19:21:47 +0300

    notifications (only show, no actions)

---
+++ application/helpers/dates_helper.php
@@ -0,0 +1,35 @@
+<?php
+
+function displayDate($date) {
+    $intervalle = date_diff(new DateTime($date), new DateTime());
+    $d = $intervalle->d;
+    $base_str = "il y a %s %s(s)";
+
+    if ($d > 6) {
+        return $date;
+    }
+
+    if($d > 0) {
+        return sprintf($base_str, $d, "jour");
+    }
+
+    $h = $intervalle->h;
+
+    if ($h > 0 ) {
+        return sprintf($base_str, $h, "heure");
+    }
+
+    $m = $intervalle->i;
+
+    if ($m > 0) {
+        return sprintf($base_str, $m, "minute");
+    }
+
+    $s = $intervalle->s;
+
+    if ($s > 10) {
+        return sprintf($base_str, $s, "seconde");
+    }
+
+    return "a l'instant";
+}
--- application/models/Client.php
+++ application/models/Client.php
@@ -8,17 +8,33 @@
         public function get_last_messages($id_client)
         {
             $sql = "select * from v_message where id_client_sender=$id_client or id_client_receiver=$id_client order by date_envoye desc limit 5";
-            $get_id_client = $this->db->query($sql); 
-    
+            $get_id_client = $this->db->query($sql);
+
             return $get_id_client->result_array();
         }
 
         public function get_last_notifications($id_client)
         {
-            $sql = "select * from v_notifications where id_client=$id_client order by date_envoye limit 5";
-            $get_last_notification = $this->db->query($sql);
+            $sql = "select * from v_notifications where id_client_receiver=$id_client order by date_envoye limit 5";
+            $lasts = $this->db->query($sql)->result_array();
+
+            for ($i = 0; $i < count($lasts); $i++){
+                if($lasts[$i]['type'] === 'commentaire') {
+                    $lasts[$i]['description'] = $this
+                        ->db
+                        ->query("select titre from publication where id_publication=(select id_publication from commentaire where id_commentaire=".$lasts[$i]['id_message'].")")
+                        ->row_array()['titre'];
+                }
+
+                else {
+                    $lasts[$i]['description'] = $this
+                        ->db
+                        ->query("select message_texte from message where id_message=".$lasts[$i]['id_message'])
+                        ->row_array()['message_texte'];
+                }
+            }
     
-            return $get_last_notification->result_array();
+            return $lasts;
         }
 
         public function insert($first_name,$last_name,$date_birth,$phone,$email,$password)
@@ -26,5 +42,6 @@
             $sql = "insert into Client(first_name,last_name,date_birth,phone,email,password) values('".$first_name."','".$last_name."','".$date_birth."','".$phone."','".$email."',md5('".$password."'));";
             $this->db->query($sql);
         }
+
     }
 ?>
