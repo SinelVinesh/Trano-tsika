624b1f6b33f260c543a97c671218ce42db4c65d5
TsinjoAH <52tsinjo@gmail.com>
2022-07-12 05:14:30 +0300

    search correction

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -653,6 +653,7 @@
     <script src="<?= base_url() ?>custom-assets/js/message.js"></script>
 
     <script>
+
         <?php if (!$_SESSION["abonnement"]) { ?>
             $("#abonnement").modal("show");
         <?php } ?>
@@ -665,10 +666,11 @@
         $("#pay_abonnement_btn").click(()=>{
             $("#go-abonnement").trigger("click");
         });
-        
+
         $("#continue-free").click(() => {
             $("#abonnement").modal("hide");
-        })
+        });
+
     </script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
